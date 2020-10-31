6d8006ee2e0169c0eb3177fdce8f33c35e112bb5
Matthew <matvey.w2015@mail.ru>
2020-10-31 22:51:04 +0400

    add table component

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import axios from 'axios'
 import {
   Container,
   createStyles,
@@ -12,6 +13,12 @@ import {
   FormControl,
   InputLabel,
   Typography,
+  TableContainer,
+  TableHead,
+  TableRow,
+  TableCell,
+  TableBody,
+  Table,
 } from '@material-ui/core';
 
 const useStyles = makeStyles((theme: Theme) =>
@@ -37,13 +44,58 @@ const useStyles = makeStyles((theme: Theme) =>
   }),
 );
 
+function createData(name:string, calories:number, fat:number, carbs:number, protein:number) {
+  return { name, calories, fat, carbs, protein };
+}
+
+const rows = [
+  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
+  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
+  createData('Eclair', 262, 16.0, 24, 6.0),
+  createData('Cupcake', 305, 3.7, 67, 4.3),
+  createData('Gingerbread', 356, 16.0, 49, 3.9),
+];
+
 function App() {
   const classes = useStyles();
+  const [data, setData] = React.useState()
+
+  React.useEffect(()=> {
+    axios.get('https://min-api.cryptocompare.com/data/price?fsym=BTC&tsyms=USD,JPY,EUR')
+    .then(({data}) => {
+      const coins = data.Data
+    })
+  }, [])
   return (
     <Container className={classes.root} maxWidth="lg">
       <Paper elevation={3}>
         <Grid item xs={8}>
-          <Paper className={classes.paper}></Paper>
+          <TableContainer component={Paper}>
+      <Table className={classes.table} aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell>Dessert (100g serving)</TableCell>
+            <TableCell align="left">Calories</TableCell>
+            <TableCell align="left">Calories</TableCell>
+            <TableCell align="left">Calories</TableCell>
+            <TableCell align="left">Calories</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {rows.map((row) => (
+            <TableRow>
+              <TableCell component="th" scope="row">
+                {row.name}
+              </TableCell>
+              <TableCell align="left">{row.calories}</TableCell>
+              <TableCell align="left">{row.fat}</TableCell>
+              <TableCell align="left">{row.carbs}</TableCell>
+              <TableCell align="left">{row.protein}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
         </Grid>
         <Grid item xs={4}>
           <Paper className={classes.paper}>
@@ -71,7 +123,7 @@ function App() {
 
                   <MenuItem value={20}>Twenty</MenuItem>
                 </Select>
-              </FormControl>
+              </FormControl> 
             </div>
             <Typography variant="h5" component='h5'>77.81 Российский рубль</Typography>
           </Paper>
