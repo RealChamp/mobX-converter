703342acd49f1d0b2bae9b4b467b16b1b000e04a
Matthew <matvey.w2015@mail.ru>
2020-10-31 21:47:15 +0400

    add input currency component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,84 @@
-import React from 'react'
+import React from 'react';
+import {
+  Container,
+  createStyles,
+  Grid,
+  makeStyles,
+  MenuItem,
+  Paper,
+  Select,
+  TextField,
+  Theme,
+  FormControl,
+  InputLabel,
+  Typography,
+} from '@material-ui/core';
 
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      padding: theme.spacing(10),
+    },
+    paper: {
+      padding: theme.spacing(2),
+      textAlign: 'center',
+      color: theme.palette.text.secondary,
+    },
+    cryptoInputBox: {
+      marginBottom: 20,
+      marginTop: 20,
+    },
+    currencyInput: {
+      minWidth: 'calc(70%-10px)',
+    },
+    currencyType: {
+      minWidth: '30%',
+    },
+  }),
+);
 
-function App() { 
-    return (
-        <div>
-            <h1>Hello Tsx World</h1>
-        </div>
-    ) 
+function App() {
+  const classes = useStyles();
+  return (
+    <Container className={classes.root} maxWidth="lg">
+      <Paper elevation={3}>
+        <Grid item xs={8}>
+          <Paper className={classes.paper}></Paper>
+        </Grid>
+        <Grid item xs={4}>
+          <Paper className={classes.paper}>
+            <div className={classes.cryptoInputBox}>
+              <FormControl className={classes.currencyInput}>
+                <TextField fullWidth label="Сумма" />
+              </FormControl>
+              <FormControl className={classes.currencyType}>
+                <InputLabel shrink>Валюта</InputLabel>
+                <Select value={10}>
+                  <MenuItem value={10}>Ten</MenuItem>
+
+                  <MenuItem value={20}>Twenty</MenuItem>
+                </Select>
+              </FormControl>
+            </div>
+            <div className={classes.cryptoInputBox}>
+              <FormControl className={classes.currencyInput}>
+                <TextField fullWidth label="Сумма" />
+              </FormControl>
+              <FormControl className={classes.currencyType}>
+                <InputLabel shrink>Валюта</InputLabel>
+                <Select value={10}>
+                  <MenuItem value={10}>Ten</MenuItem>
+
+                  <MenuItem value={20}>Twenty</MenuItem>
+                </Select>
+              </FormControl>
+            </div>
+            <Typography variant="h5" component='h5'>77.81 Российский рубль</Typography>
+          </Paper>
+        </Grid>
+      </Paper>
+    </Container>
+  );
 }
 
-export default App
+export default App;
